c729073ca832999e5567eb22a7c85638ef9044cc
Ilya Breitburg <ilya.breytburg@gmail.com>
2024-10-02 03:18:54 +0200

    Initial Commit

---
+++ CarbonMonitor/CarbonMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbonMonitorApp.swift
+//  CarbonMonitor
+//
+//  Created by Ilia Breitburg on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarbonMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarbonMonitorTests/CarbonMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarbonMonitorTests.swift
+//  CarbonMonitorTests
+//
+//  Created by Ilia Breitburg on 02/10/2024.
+//
+
+import Testing
+@testable import CarbonMonitor
+
+struct CarbonMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
